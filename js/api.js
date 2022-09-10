66731152373fcc00ec1b9936861d62714b8db477
jawad alsayed <jawadalsayed@jawads-MacBook-Pro.local>
2022-09-11 01:30:09 +0300

    cleaning and commenting

--- js/api.js
+++ js/api.js
@@ -3,6 +3,7 @@ const img1 = document.getElementById("dog_image1");
 const img2 = document.getElementById("dog_image2");
 const image_file = "https://dog.ceo/api/breeds/image/random";
 
+// getting the images
 fetch(image_file)
 .then (x => x.json())
 .then (image => {
@@ -10,6 +11,7 @@ fetch(image_file)
     img2.src = image.message;
 });
 
+// on submit function
 const submite = (element) => {
     element.addEventListener('click', (event) => {
         const name = document.getElementById("input").value;
@@ -20,6 +22,7 @@ const submite = (element) => {
 
         document.getElementById("name").innerHTML = "Name: " + name;
 
+        // getting the gender details
         fetch(gender_file)
         .then (x => x.json())
         .then (gender => {
@@ -31,6 +34,7 @@ const submite = (element) => {
             document.getElementById("gender_count").innerHTML = "Count: " + count;
         });
 
+        // getting the age details
         fetch(age_file)
         .then (x => x.json())
         .then (ages => {
@@ -39,7 +43,7 @@ const submite = (element) => {
             document.getElementById("age").innerHTML = "Age: " + age;
             document.getElementById("age_count").innerHTML = "Count: " + count;
         });
-
+        // getting the nationality details
         fetch(nationality_file)
         .then (x => x.json())
         .then (nationality => {
@@ -55,44 +59,6 @@ const submite = (element) => {
     })
 }
 
-// const image = "https://dog.ceo/api/breeds/image/random";
-// const gender_file = "https://api.genderize.io/?name=" + name; 
-// const age_file = "https://api.agify.io/?name=jawad";
-// const nationality_file = "https://api.nationalize.io/?name=jawad";
-
+// calling on submit function
 submite(document.getElementById("submit"))
 
-
-
-// fetch(gender_file)
-// .then (x => x.json())
-// .then (gender => {
-//     gender_type = gender.gender;
-//     probabilty = gender.probability;
-//     count = gender.count;
-//     document.getElementById("gender").innerHTML = gender_type;
-//     document.getElementById("gender_probability").innerHTML = probabilty;
-//     document.getElementById("gender_count").innerHTML = count;
-// });
-
-// fetch(age_file)
-// .then (x => x.json())
-// .then (ages => {
-//     age = ages.age;
-//     count = ages.count;
-//     document.getElementById("age").innerHTML = age;
-//     document.getElementById("age_count").innerHTML = count;
-// });
-
-// fetch(nationality_file)
-// .then (x => x.json())
-// .then (nationality => {
-//     nationality_1 = nationality.country[0].country_id;
-//     probabilty_1 = nationality.country[0].probability;
-//     nationality_2 = nationality.country[1].country_id;
-//     probabilty_2 = nationality.country[1].probability;
-//     document.getElementById("country1").innerHTML = nationality_1;
-//     document.getElementById("nationality_probability1").innerHTML = probabilty_1;
-//     document.getElementById("country2").innerHTML = nationality_2;
-//     document.getElementById("nationality_probability2").innerHTML = probabilty_1;
-// });
