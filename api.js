e9349f19d7694cbb75201873bad26564de5d9d5f
jawad alsayed <jawadalsayed@jawads-MacBook-Pro.local>
2022-09-10 12:08:46 +0300

    linking age api

--- api.js
+++ api.js
@@ -1,7 +1,12 @@
 
 const name = document.getElementById("input");
 
+const image = "https://dog.ceo/api/breeds/image/random";
 const gender_file = "https://api.genderize.io/?name=jawad";
+const age_file = "https://api.agify.io/?name=jawad";
+const nationality_file = "https://api.nationalize.io/?name=jawad";
+
+
 
 fetch(gender_file)
 .then (x => x.json())
@@ -12,4 +17,13 @@ fetch(gender_file)
     document.getElementById("gender").innerHTML = gender_type
     document.getElementById("gender_probability").innerHTML = probabilty
     document.getElementById("gender_count").innerHTML = count
+});
+
+fetch(age_file)
+.then (x => x.json())
+.then (ages => {
+    age = ages.age
+    count = ages.count
+    document.getElementById("age").innerHTML = age
+    document.getElementById("age_count").innerHTML = count
 });
